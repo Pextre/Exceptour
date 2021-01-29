7f0ee6eb2597e760618c1d53fa1e7eb133ffc316
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2021-01-29 15:20:18 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const selectAllElements = (e) => document.querySelectorAll(e);
 
 const navToggle = selectElement(".nav__menu");
 const navLinks = selectAllElements(".nav-list__item");
-
+const testemunhos = selectAllElements(".testemunho");
 //  MENU ----------------------------------------------------------------
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav__open");
@@ -20,15 +20,44 @@ navLinks.forEach((link) => {
 //  Carrossel ----------------------------------------------------------------
 
 document.addEventListener("DOMContentLoaded", function () {
-    let splide = new Splide("#splide", {
+    let sobreSplide = new Splide("#splide--sobre", {
         type: "loop",
         direction: "ttb",
         height: "550px",
+        fixedHeight: "550px",
         arrowPath:
             "m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z",
         autoplay: true,
         resetProgress: true,
+        perPage: 1,
     });
-    splide.mount();
+    sobreSplide.mount();
+
+    let testemunhosSplide = new Splide("#splide--testemunhos", {
+        type: "loop",
+        perPage: 2,
+        focus: "center",
+        fixedHeight: "350px",
+        gap: "1em",
+        autoplay: true,
+        resetProgress: true,
+        autoWidth: true,
+        interval: 3000,
+        speed: 650,
+    });
+
+    testemunhosSplide.mount();
+    testemunhos[0].classList.add("testemunho--ativo");
+
+    testemunhosSplide.on("move", function (newIndex, oldIndex, destIndex) {
+        testemunhos[0].classList.remove("testemunho--ativo");
+        testemunhos.forEach((testemunho, index) => {
+            if (index === newIndex) {
+                testemunho.style.opacity = 1;
+            } else {
+                testemunho.style.opacity = 0.35;
+            }
+        });
+    });
 });
 // ------------------------------------------------------------------
