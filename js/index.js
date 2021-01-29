8444d3f7490d951893ae0100843e09ed923eb087
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2021-01-28 23:22:04 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,9 +4,7 @@ const selectAllElements = (e) => document.querySelectorAll(e);
 
 const navToggle = selectElement(".nav__menu");
 const navLinks = selectAllElements(".nav-list__item");
-const carrosselItens = selectAllElements(".carrossel__item");
-const carrosselLinks = selectAllElements(".carrossel__link");
-const tempoTotalCarrossel = 10000; // 10 segundos
+
 //  MENU ----------------------------------------------------------------
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav__open");
@@ -19,26 +17,12 @@ navLinks.forEach((link) => {
 });
 // ------------------------------------------------------------------
 
-let slideAtual = 0;
-girarCarrossel();
+//  Carrossel ----------------------------------------------------------------
 
-function girarCarrossel() {
-    let i;
-    for (i = 0; i < carrosselItens.length; i++) {
-        carrosselItens[i].classList.remove("carrossel__item--active");
-    }
-
-    carrosselItens.forEach((elemeto, index) => {
-        carrosselItens[index].classList.remove("carrossel__item--active");
-        carrosselLinks[index].classList.remove("carrossel__link--active");
-    });
-
-    slideAtual++;
-    if (slideAtual > carrosselItens.length) {
-        slideAtual = 1;
-    }
-
-    carrosselLinks[slideAtual - 1].classList.add("carrossel__link--active");
-    carrosselItens[slideAtual - 1].classList.add("carrossel__item--active");
-    setTimeout(girarCarrossel, 5000);
-}
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide("#splide", {
+        direction: "ttb",
+        height: "90vh",
+    }).mount();
+});
+// ------------------------------------------------------------------
